70b370362d75720b3fb0e1d6cc8158f9445e9708
maha <mahag.math@gmail.com>
2020-03-30 18:37:10 -0700

    Create Sample.py

---
+++ Sample.py
@@ -0,0 +1,11 @@
+ # importing libraries
+   import time
+   
+   # printing and calling functions
+   print(time.ctime())
+   
+   # variables and math
+   calculation = (1 * 2 * 3) / 2  
+   
+   # strings
+   print('I've done some math!', calculation)
